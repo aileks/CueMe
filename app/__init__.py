62848ffa62fa9a770e91df12812c530a42d6bb9b
Aaliyah Harper <aeverly14@pm.me>
2025-05-06 11:54:20 -0400

    get index.html to properly serve

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 import os
 
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, send_from_directory
 from flask_cors import CORS
 from flask_migrate import Migrate
 
+# from flask_login import LoginManager
 from .config import Config
 from .models import db
 
 app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
 app.config.from_object(Config)
+
+# login = LoginManager(app)
+# login.login_view = "auth.unauthorized"
+
+
+# @login.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
+
+
+db.init_app(app)
 Migrate(app, db)
 CORS(app)
 
@@ -26,7 +38,7 @@ def https_redirect():
 @app.route("/<path:path>")
 def react_root(path):
     if path == "favicon.ico":
-        return app.send_from_directory("public", "favicon.ico")  # pyright: ignore
+        return send_from_directory("public", "favicon.ico")
     return app.send_static_file("index.html")
 
 
