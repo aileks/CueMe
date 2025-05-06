5c7f863490a8a906fc2a74e579643c203e2cf9a3
Aaliyah Harper <aeverly14@pm.me>
2025-05-06 15:29:46 -0400

    starter logic for auth routes added

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,23 @@
 import os
 import uuid
 
-from flask import Flask, redirect, request, send_from_directory
+from flask import Flask, Response, redirect, request, send_from_directory
 from flask_cors import CORS
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import generate_csrf
 
 from .config import Config
 from .models import User, db
-
+from .routes import auth_bp
 
 app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.config.from_object(Config)
 
 login = LoginManager()
 login.init_app(app)
-login.login_view = "auth.unauthorized"  # pyright: ignore
+login.login_view = "auth.unauthorized"  # type: ignore
 
 
 @login.user_loader
@@ -39,22 +41,34 @@ CORS(app)
 
 
 @app.before_request
-def https_redirect():
+def https_redirect() -> Response | None:
     if os.environ.get("FLASK_ENV") == "prod":
         if request.headers.get("X-Forwarded-Proto") == "http":
             url = request.url.replace("http://", "https://", 1)
             code = 301
-            return redirect(url, code=code)
+            return redirect(url, code=code)  # pyright: ignore
+
+
+@app.after_request
+def inject_csrf_token(response: Response) -> Response:
+    response.set_cookie(
+        "csrf_token",
+        generate_csrf(),
+        secure=True if os.environ.get("FLASK_ENV") == "production" else False,
+        samesite="Strict" if os.environ.get("FLASK_ENV") == "production" else None,
+        httponly=True,
+    )
+    return response
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
-def react_root(path):
+def react_root(path) -> Response:
     if path == "favicon.ico":
         return send_from_directory("public", "favicon.ico")
     return app.send_static_file("index.html")
 
 
 @app.errorhandler(404)
-def not_found(e):
+def not_found(e) -> Response:
     return app.send_static_file("index.html")
