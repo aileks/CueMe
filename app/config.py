2a9452c305ce9cf5847298bb65fbec750fc6f0cb
Aaliyah Harper <aeverly14@pm.me>
2025-05-06 11:57:13 -0400

    clean up config.py

--- app/config.py
+++ app/config.py
@@ -5,7 +5,9 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     FLASK_RUN_PORT = os.environ.get("FLASK_RUN_PORT")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(
-        "postgres://", "postgresql://"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    if SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
+
     SQLALCHEMY_ECHO = True
