a221aef175bb4be5a85494073ce379096d22af27
Aaliyah Harper <aeverly14@pm.me>
2025-05-12 11:23:54 -0400

    create songs model and migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import SCHEMA, db, environment
 from .playlist import Playlist
+from .song import Song
 from .user import User
---
+++ app/models/song.py
@@ -0,0 +1,25 @@
+import uuid
+
+from .db import SCHEMA, UUIDColumnType, db, environment
+
+
+class Song(db.Model):
+    __tablename__ = "songs"
+
+    if environment == "prod":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(UUIDColumnType, primary_key=True, default=uuid.uuid4)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50))
+
+    playlists = db.relationship("PlaylistSong", back_populates="song")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "artist": self.artist,
+            "genre": self.genre,
+        }
